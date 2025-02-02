62d8125bd43323f12cec9f7ed9d359b513d1ea74
Subrat <subratpathak95@gmail.com>
2025-02-02 17:27:24 +0530

    starting

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello World From React!");
+
+const root = ReactDOM.createElement(document.getElementById("root"));
+
+root.render(heading);
