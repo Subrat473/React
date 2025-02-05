c3a892260a5b0e701e1c4a8795f0ecde2a02ab10
Subrat <subratpathak95@gmail.com>
2025-02-05 14:32:54 +0530

    add

--- app.js
+++ app.js
@@ -8,22 +8,59 @@
  * 
  * ReactElement(Object) => HTML(Browser Understands)
  */
+ import React from "react";
+ import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    React.createElement("div", { id: "child"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child"},[
-        React.createElement("h1", {}, "I'm an h1 tag"),
-        React.createElement("h2", {}, "I'm an h2 tag"),
-    ])
-);
 
-console.log(parent); // object
 
-const root = ReactDom.createElement(document.getElementById("root"));
 
-root.render(parent);
+
+
+ // What is a react element 
+ // React.createElement is a js object then it renders as an html element 
+
+ const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+
+ console.log(heading);
+
+ // JSX - is not HTML but it just look like HTML 
+
+ // (transpiled before it reaches the JS Engines ) it's getting done by the - PARCEL by babel 
+
+ //  jsx converts into react.createElement then it converts into reactelement and then the js object then htmlelement(render)
+
+ const jsxheading = <h1 id="heading">Namaste React using JSX</h1>;
+
+ console.log(jsxheading);
+
+ 
+
+
+ // Component Composition 
+
+ function HeadingComponent() {
+    <div id="container">
+        {}
+        <h1 className="healing">Namaste React Functional Component</h1>
+    </div>;
+}
+ 
+// React Component 
+// Class Based Component - old way of writing 
+// Functional Component - that's the new way of writing component 
+
+
+const HeadingComponent = () => { 
+    <h1 className="heading"> Namaste React Functional Component</h1>
+};
+
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+
+ root.render(jsxheading); 
+
+
+
+
